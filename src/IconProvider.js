f7a5816aa3e47341fe54edb1ec337337d86522b9
dennisja <dennisjjagwe@gmail.com>
2018-11-14 15:47:25 +0300

    Seperate front and backend code

--- src/IconProvider.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { IconContext } from 'react-icons';
-
-export default ({ children }) => (
-  <IconContext.Provider value={{ style: { verticalAlign: 'text-bottom' } }}>
-    {children}
-  </IconContext.Provider>
-);
--- src/context.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TweetsSortContext = React.createContext({ setTweets: () => {} });
-
-const SetThemeContext = React.createContext({ setTheme: () => {} });
-
-export { TweetsSortContext, SetThemeContext };
